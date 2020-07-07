457f2b4224b99c92ea53434ecdc716c211d4111d
emerykurt <emerylumsden@gmail.com>
2020-07-07 17:59:03 -0400

    Updated ActiveRecord

--- app/models/eatery.rb
+++ app/models/eatery.rb
@@ -1,4 +1,4 @@
 class Eatery < ActiveRecord::Base
-    has_many :checkins_and_ratings
-    has_many :users, through: :checkins_and_ratings  
+    has_many :ratings
+    has_many :users, through: :ratings  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User  < ActiveRecord::Base
     has_many :checkins_and_ratings
-    has_many :eateries, through: :checkins_and_ratings
+    has_many :eateries, through: :ratings
     has_secure_password
     validates :first_name, presence: true
     validates :last_name, presence: true
